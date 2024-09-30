c280325c124eb1911f19becc0d44592d85d2a63d
zhen1007 <172377045+zhen1007@users.noreply.github.com>
2024-09-30 09:05:14 -0400

    initial commit

---
+++ data/index.ts
@@ -0,0 +1,230 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Education", link: "#education" },
+  { name: "Experience", link: "#experience" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve and learn new technology",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "GiftLink - Full-stack app for item giveaways",
+    des: "Connects users who wish to give away household items they no longer need with users who enjoy recycling and prefer to find free household items to match their tastes rather than purchasing new ones",
+    img: "/giftlink.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "#",
+  },
+  {
+    id: 2,
+    title:
+      "Dealership - Website for car dealership reviews by branch and state.",
+    des: "Creates a website for customers to view and submit dealership reviews by branch and state.",
+    img: "/dealership.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Fanimal - Tickets to Live Events - Experience More",
+    des: "Tickets to concerts, festivals, sporting events & more. Use Fanimal Groups to buy with your friends",
+    img: "/fanimal.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://fanimal.com/",
+  },
+  {
+    id: 4,
+    title: "React Native Piano Keyboard",
+    des: "Piano Keyboard component written in TypeScript Expo",
+    img: "/rn-piano.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://zhen1007.github.io/React-Native-PianoKeyboard/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Zhen was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Zhen's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Zhen is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Zhen was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Zhen's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Zhen is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Zhen was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Zhen's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Zhen is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Zhen was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Zhen's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Zhen is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Zhen was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Zhen's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Zhen is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "React Native Developer - FFS",
+    desc: "Designed, developed, and maintained high-quality mobile applications for clients using React Native",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Frontend Developer - Adamnite",
+    desc: "Responsible for crafting intuitive and responsive user interfaces for our blockchain-based applications",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Web & Mobile Developer - KleenEdge",
+    desc: "Spearheaded the development of mobile applications using React Native, contributing to the company's digital transformation in the cleaning industry",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Full Stack Developer - Fanimal",
+    desc: "Implemented new features and functionality in the app, including event discovery, ticket purchasing, and user profile management",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 4,
+    title: "Full Stack Engineer - FiveStarsMobi",
+    desc: "Developed and maintained web applications using React, Laravel, and Django",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/zhen1007",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "#",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "#",
+  },
+];
